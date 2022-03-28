fa642f53e3b8ca02dbf580fdf744256041f085a4
clarkzjw <i@jinwei.me>
2022-03-27 20:56:34 -0700

    + add fairness demo results

--- results/2. fairness/plot_playback_bitrate.py
+++ results/2. fairness/plot_playback_bitrate.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import json
 
-alg = ["basic", "netflix", "sara"]
-for j in range(3):
+alg = ["netflix"]
+for j in range(len(alg)):
     fig, axs = plt.subplots(4, 1, constrained_layout=True)
     fig.suptitle("Playback bitrate w.r.t playback time")
-    for i in range(4):
+    for i in range(1):
         filename = "sw1c{}/ASTREAM_LOGS/ASTREAM_{}.json".format(i+1, alg[j])
 
         bitrate = []
@@ -13,7 +13,7 @@ for j in range(3):
             data = json.load(json_file)
             segment_info = data["segment_info"]
             for info in segment_info:
-                bitrate.append(info[1]/1000.0)
+                bitrate.append(int(info[1])/1000.0)
         axs[i].plot(bitrate)
         axs[i].set_ylabel("bitrate/kbps")
         axs[i].set_title(alg[j])
